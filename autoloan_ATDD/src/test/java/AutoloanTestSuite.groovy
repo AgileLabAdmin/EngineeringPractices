6a3908ba5c7beebf7bc34ddfc663aa57f29f5ef7
DevAgileLabAdmin <DevAgileLabAdmin@cognizant.com>
2017-11-02 13:14:24 -0400

    Projects

--- autoloan_ATDD/src/test/java/AutoloanTestSuite.groovy
+++ autoloan_ATDD/src/test/java/AutoloanTestSuite.groovy
@@ -6,9 +6,6 @@ import junit.textui.TestRunner
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
-/**
- * Created by qualityshepherd on 4/26/14.
- */
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
